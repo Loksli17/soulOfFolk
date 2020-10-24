5607095ad72c7288829640940a7cc20cb697a296
RobinLoskli <ami0504@mail.ru>
2020-10-24 15:40:36 +1000

    firstCommit

---
+++ index.js
@@ -0,0 +1,17 @@
+const
+    express    = require('express'),
+    fileUpload = require('express-fileupload'),
+    session    = require('express-session'),
+    cookie     = require('cookie-parser'),
+    hbs        = require('hbs'),
+    helmet     = require('helmet'),
+    bodyParser = require('body-parser'),
+    flash      = require('connect-flash'),
+    csurf      = require('csurf');
+
+const
+    config = require('./config');
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
